718006d8ba12e4c50d5aeda794c3cf91879bf68d
jackmock <jack.r.mock@gmail.com>
2025-01-22 13:50:45 -0600

    added boilerplate code, requirements.txt dependency doc, .gitignore file, etc

---
+++ main.py
@@ -0,0 +1,15 @@
+from constants import *
+
+import pygame
+
+def main():
+    print("Starting asteroids!")    
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    
+
+
+if __name__== "__main__":
+    main()
+
